ae5797a04721cae2615d20fcd8b489c18fe39043
tatevar <tatevar93@gmail.com>
2021-07-06 18:37:00 +0400

    Ex2 fix done

--- src/com/company/ArrayPart2Ex2.java
+++ src/com/company/ArrayPart2Ex2.java
@@ -13,7 +13,7 @@ public class ArrayPart2Ex2 {
         Random random = new Random();
         for (int[] row : matrix) {
             for (int i = 0; i < row.length; i++) {
-                row[i] = random.nextInt(50) + 1;
+                row[i] = random.nextInt(5) + 1;
             }
         }
         for (int[] row : matrix) {
@@ -22,26 +22,29 @@ public class ArrayPart2Ex2 {
             }
             System.out.println();
         }
-        int summ = 0;
+        int amount = 1;
         for (int i = 0; i < matrix.length; i++) {
             for (int j = i; j < matrix.length; j++) {
-                summ += matrix[i][j];
+                amount *= matrix[i][j];
                 break;
             }
         }
 
-        int summ2 = 0;
+        int amount1 = 1;
         for (int i = matrix.length - 1; i >= 0; i--) {
             for (int j = matrix.length - 1 - i; j < matrix.length; j++) {
-                summ += matrix[i][j];
+                amount1 *= matrix[i][j];
                 break;
             }
         }
-        if (summ > summ2) {
-            System.out.println("произведение элементов главной диагонали больше " + summ + summ2);
+        if (amount > amount1) {
+            System.out.println("произведение элементов главной диагонали больше " + amount + " " + amount1 );
         }
-        if (summ < summ2) {
-            System.out.println("произведение элементов главной диагонали меньше " + summ + summ2);
+        if (amount < amount1) {
+            System.out.println("произведение элементов главной диагонали меньше " + amount + " " + amount1 );
+        }
+        if (amount == amount1){
+            System.out.println("бла=бла " + amount + amount1);
         }
     }
 }
