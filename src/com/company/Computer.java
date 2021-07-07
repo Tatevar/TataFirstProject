260f7d7ba19c750e109502ef9081f93d24951ff6
tatevar <tatevar93@gmail.com>
2021-07-07 19:20:46 +0400

    Fixed

--- src/com/company/Computer.java
+++ src/com/company/Computer.java
@@ -17,11 +17,58 @@
 
 package com.company;
 
-public class Computer {
-    public int ram ;
-    public int cpu ;
-    public int hdd ;
-    public String turnOnOff;
-}
+import java.util.Scanner;
 
+public class Computer {
+    public static void display() {
+        int ram = 90;
+        int cpu = 30;
+        int hdd = 876;
+        String turnOnOff = "yes";
+        System.out.println(cpu);
+        System.out.println(hdd);
+        System.out.println(ram);
+        System.out.println(turnOnOff);
+        System.out.println();
+    }
+    public static void  turnOn (){
+
+        int UnknownNumber, UserNumber;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("ЗАДАЧА: Угадать число (от 0 до 1).");
+
+        UnknownNumber = (int)Math.floor(Math.random() * 1);
+
+        System.out.print("Введите ваше число: ");
+
+        UserNumber = input.nextInt();
+
+        if (UserNumber == UnknownNumber)
+            System.out.println(" Ваш компьютер включен");
+
+        else if (UserNumber != UnknownNumber)
+            System.out.println("сбой, " +  "Ваш компьютер сгорел ");
+    }
+
+    public static void  turnOff (){
+
+        int UnknownNumber, UserNumber;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("ЗАДАЧА: Угадать число (от 0 до 1).");
+
+        UnknownNumber = (int)Math.floor(Math.random() * 1);
+
+        System.out.print("Введите ваше число: ");
+
+        UserNumber = input.nextInt();
+
+        if (UserNumber == UnknownNumber)
+            System.out.println(" Ваш компьютер выключен");
+
+        else if (UserNumber != UnknownNumber)
+            System.out.println( "При превышении лимита ресурса, ваш компьютер сгорел ");
+    }
+}
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,59 +6,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Computer computer = new Computer();
-        computer.cpu = 30;
-        computer.hdd = 8;
-        computer.ram = 300;
-        computer.turnOnOff = "yes";
-        System.out.println(computer.cpu );
-        System.out.println( computer.hdd);
-        System.out.println( computer.ram);
-        System.out.println( computer.turnOnOff);
-        System.out.println();
-        turnOn();
-        turnOff();
-    }
-    public static void  turnOn (){
+        Computer.display();
+        Computer.turnOn();
+        Computer.turnOff();
 
-        int UnknownNumber, UserNumber;
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("ЗАДАЧА: Угадать число (от 0 до 1).");
-
-        UnknownNumber = (int)Math.floor(Math.random() * 1);
-
-            System.out.print("Введите ваше число: ");
-
-            UserNumber = input.nextInt();
-
-            if (UserNumber == UnknownNumber)
-                System.out.println(" Ваш компьютер включен");
-
-            else if (UserNumber != UnknownNumber)
-                System.out.println("сбой, " +  "Ваш компьютер сгорел ");
-    }
-
-    public static void  turnOff (){
-
-        int UnknownNumber, UserNumber;
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("ЗАДАЧА: Угадать число (от 0 до 1).");
-
-        UnknownNumber = (int)Math.floor(Math.random() * 1);
-
-        System.out.print("Введите ваше число: ");
-
-        UserNumber = input.nextInt();
-
-        if (UserNumber == UnknownNumber)
-            System.out.println(" Ваш компьютер выключен");
-
-        else if (UserNumber != UnknownNumber)
-            System.out.println( "При превышении лимита ресурса, ваш компьютер сгорел ");
     }
 }
+
 
 
 
