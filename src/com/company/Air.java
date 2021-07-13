38170f58e1633d5bb8a4dd09f06834bf32ce6438
tatevar <tatevar93@gmail.com>
2021-07-13 18:57:48 +0400

    inheritance

--- src/com/company/Air.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Air extends Transport {
-    private int wingSpanMetres = 100;
-    private int minLengthMetres = 250;
-}
-//Размах крыльев (м)
-//- Минимальная длина взлётно-посадочной полосы для взлёта
